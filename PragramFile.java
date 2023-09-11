4244aa3320f2a066a69628aadcdbebb6e4eca4e5
Naveena Andril <naveena.rajeei@gmail.com>
2023-09-11 09:28:01 +0530

    Added first program for git commands

---
+++ PragramFile.java
@@ -0,0 +1,28 @@
+package Practicegitcommands;
+
+public class PragramFile {
+
+    public void add(int a, int b){
+        int c = a + b;
+        System.out.println("The addition of given values is " + c);
+        }
+        
+        public void add(double a, double b){
+        double c = a * b;
+        System.out.println("The addition of given values is " + c);
+        }
+        
+        
+        
+        
+            public static void main(String[] args) {
+        
+                PragramFile mainclass = new PragramFile();
+                
+               mainclass.add(3, 6);
+               mainclass.add(3.4, 5.6); 
+                
+            }
+        
+        }
+
